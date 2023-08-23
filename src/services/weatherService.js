e4cf4bb3c9228172a466690693ea71962ae63d0a
Meenakshi2002 <82953232+Meenakshi2002@users.noreply.github.com>
2023-08-23 22:53:33 +0530

    Update weatherService.js

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "c36d0a9dc2be253058d9450f8738c94d";
+const API_KEY = "";//insert ur api key 
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
